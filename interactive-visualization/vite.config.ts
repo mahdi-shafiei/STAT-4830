288c0810170ab8b882849bb690fb55974a6a81c9
damek <damek@users.noreply.github.com>
2025-04-18 11:47:47 -0400

    chore(visualization): remove unused files and configurations for a cleaner project structure

--- interactive-visualization/vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/STAT-4830/', // Crucial for GitHub Pages routing! Matches repo name.
-})
